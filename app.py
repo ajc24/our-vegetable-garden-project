05f71465c7a92f90119da2ca649b00c21d4f86ad
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-28 16:07:29 +0000

    add photo file implementation in post request

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import json
 import secrets
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import file_manager
 
 # python setup.py install
 # $FLASK_APP=c:\Projects\our-vegetable-garden-project\app.py $FLASK_ENV=development flask run
+
+FILES_DIR = "/files"
 
 APP = Flask(__name__)
 
@@ -26,10 +30,36 @@ def render_add_a_plant_page():
 
 @APP.route("/submitaddaplant", methods = ["POST"])
 def submit_add_a_plant():
+    # Take in the plant name and create the file directory name from it
     name = request.form["name"]
+    directory_name = name.lower().replace(" ", "-")
+    # Take in the variety name and create the file image name from it
     variety = request.form["variety"]
-    print(name)
-    print(variety)
+    if variety == "":
+        variety = "Default"
+    temp_file_name = name.lower().replace(" ", "-") + "-" + variety.lower().replace(" ", "-") + ".png"
+    # Take in the photo for this plant
+    photo = request.files["photo"]
+    # Debugging output
+    print("NAME: " + name)
+    print("DIR NAME: " + directory_name)
+    print("FILE NAME: " + temp_file_name)
+    print("VARIETY: " + variety)
+    # Create the directory in which to store the image file for this plant
+    dir_exists = file_manager.does_directory_exist(FILES_DIR)
+    if dir_exists == False:
+        print("***** CREATE FILES DIRECTORY")
+        file_manager.create_directory(FILES_DIR)
+    dir_exists = file_manager.does_directory_exist(FILES_DIR + "/" + directory_name)
+    if dir_exists == False:
+        print("***** CREATE IMAGE FILE DIRECTORY")
+        file_manager.create_directory(FILES_DIR + "/" + directory_name)
+    final_file_name = secure_filename(temp_file_name)
+    file_exists = file_manager.does_file_exist(FILES_DIR + "/" + directory_name + "/" + final_file_name)
+    if file_exists == False:
+        print("***** CREATE NEW IMAGE FILE")
+        file_manager.saveImageFile(photo, FILES_DIR + "/" + directory_name, final_file_name)
+    print("***** DONE")
     return "RESPONSE_FROM_PYTHON"
 
 APP.config["SECRET_KEY"] = secrets.token_hex(24)
